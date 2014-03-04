d8885066fec4c3c936129d09428653f868bd86b0
Matt Soucy <msoucy@csh.rit.edu>
2014-03-04 15:06:16 -0500

    Added simple tests for actions

--- tests/test_action.py
+++ tests/test_action.py
@@ -2,36 +2,51 @@ from __future__ import print_function
 
 from fuf import ActionSet
 
-# Create a simple action set
-action = ActionSet()
+def test_realworld():
+    # Create a simple action set
+    action = ActionSet()
 
 
-# Full decorator
-# Nothing is gathered implicitly
-@action("help", "List command help")
-def action_help(args):
-    "List command help"
-    if args:
-        for arg in args:
-            if arg in action:
-                print("{0.name}:\t{0.helpmsg}".format(action[arg]))
-    else:
-        print("Available commands:", ", ".join(a for a in action))
+    # Full decorator
+    # Nothing is gathered implicitly
+    @action("help", "List command help")
+    def action_help(*args):
+        "List command help"
+        if args:
+            for arg in args:
+                if arg in action:
+                    print("{0.name}:\t{0.helpmsg}".format(action[arg]))
+        else:
+            print("Available commands:", ", ".join(a for a in action))
 
 
-# Partially applied
-# The name is specified, but the help is pulled from the docstring
-@action("verify")
-def action_verify(args):
-    "Confirm access"
-    print("Authentication valid")
+    # Partially applied
+    # The name is specified, but the help is pulled from the docstring
+    @action("verify")
+    def action_verify():
+        "Confirm access"
+        print("Authentication valid")
 
 
-# Implicit
-# The name of the command and the help are pulled from
-# the function name and docstring
-@action
-def say(args):
-    "Say a message"
-    print(" ".join(args))
+    # Implicit
+    # The name of the command and the help are pulled from
+    # the function name and docstring
+    @action
+    def say(*args):
+        "Say a message"
+        print(" ".join(args))
+
+
+def test_basic():
+    action = ActionSet()
+
+    @action
+    def add(x, y):
+        return float(x)+float(y)
+
+    @action
+    def sub(x, y):
+        return float(x)-float(y)
+
+    assert action.perform("sub 2 5") == -3, "Basic subtraction failed"
 
