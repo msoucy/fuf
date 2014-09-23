43b8df80a928a2789d64f6bdb9c00f258f1706b4
Matt Soucy <msoucy@csh.rit.edu>
2014-09-23 18:12:16 -0400

    Use PBR instead of distribute

--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
 
-from distribute_setup import use_setuptools
-use_setuptools()
-
-from setuptools import setup, find_packages
-
-version = "0.5.0"
+from setuptools import setup
 
 setup(
-    name="fuf",
-    version=version,
-    description="Funced-Up Functions",
-    classifiers=[],
-    keywords="",
-    author="Matt Soucy",
-    author_email="msoucy@csh.rit.edu",
-    url="http://github.com/msoucy/fuf",
-    license="MIT",
-    packages=find_packages(
-    ),
-    scripts=[
-        "distribute_setup.py",
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "",
-    ],
-    #TODO: Deal with entry_points
-    #entry_points="""
-    #[console_scripts]
-    #pythong = pythong.util:parse_args
-    #"""
+    setup_requires=["pbr"],
+    pbr=True
 )
+
